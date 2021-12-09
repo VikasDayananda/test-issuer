70d6246809b09c32a5ea7be98a9ccd804409c4b4
Vikas D <viky@Vikass-MacBook-Air.local>
2021-12-09 19:39:07 +0530

    initial issuer commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SampleDiv from './test-common/components/sample'
+
+function App() {
+  return (
+    <div className="App">
+     <SampleDiv value ="issuer" />
+    </div>
+  );
+}
+
+export default App;
